b2f9020aa6c5e2b7a07a74b1e8a6b1a3ab17380b
IIPAIIOBEDNIK <33659781+IIPAIIOBEDNIK@users.noreply.github.com>
2024-09-14 00:21:06 +0300

    Update Start.java

--- Start.java
+++ Start.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Start {
 
-    private static String msg = "Hello  QA_Guru!!!!!!!!!!!"
+    private static String msg = ""Hello  QA_Guru!!!!!!!!!!!""
     
     public static void main(String[] args) {
         System.out.println(msg);
