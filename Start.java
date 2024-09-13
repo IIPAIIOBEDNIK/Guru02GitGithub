a36e661ed3469d14b855cd92ae852c591fecf265
IIPAIIOBEDNIK <33659781+IIPAIIOBEDNIK@users.noreply.github.com>
2024-09-14 00:17:32 +0300

    Update Start.java

--- Start.java
+++ Start.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Hello  QA_Guru!");
+        //added new string
+        System.out.println("Hello  QA_Guru!!!!!!!!!!!");
         System.out.println(msg);
     }
 }
