279993bcf20c79bb5daab7c4df7cfbfa23ce81fc
3yd <pereponov_alexander@mail.ru>
2024-09-13 23:52:18 +0300

    hello message was changed

--- Start.java
+++ Start.java
@@ -3,7 +3,7 @@ package org.example;
 public class Start {
     public static void main(String[] args) {
 
-        System.out.println("Hello  World");
+        System.out.println("Hello  QA_Guru!");
 
 
     }
