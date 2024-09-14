513e42723140656b1760a69f09bb3f86774ac326
IIPAIIOBEDNIK <33659781+IIPAIIOBEDNIK@users.noreply.github.com>
2024-09-14 11:39:44 +0300

    Update Start.java

--- Start.java
+++ Start.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Hello  QA_Guru!");
-        System.out.println(msg);
+        System.out.println("Hello");
+        System.out.println(mg);
 
         System.out.println("Hello  QA_Guru!");
 
